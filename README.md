7055e537690bb963a5ca6affc0d8a13fbbf491fe
Felix Klein <felix@felixklein.net>
2018-04-17 22:23:22 +0200

    cleanup

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # cache-preload
 A simple utility to preload/warmup webserver and cdn caches using sourcemaps - also can take screenshots
+Writting in Python 3 using selenium and geckodriver.
+
+## Installation
+
+### Docker
+The easiest way to run this is by using the pre-built docker image [fekle/cache-preload:latest](https://hub.docker.com/r/fekle/cache-preload/)
+or building the image yourself with `./cli docker-build`. You can then run via `./cli docker-run <cmd>` 
+or simply by running:
+```bash
+docker run --rm -t --shm-size=2g \
+    --user "$(id -u):$(id -g)" \
+    -v "$(pwd):/workdir:rw" \
+    cache-preload:latest <cmd>
+```
+
+Notice the `--shm-size=2g` (shared memory size) flag - this is important, as otherwise Firefox will crash.
+
+### Local
+To install locally, use `pipenv install` and `pipenv run ./cache-preload.py`.
+For development, you can start a shell in the venv with `pipenv shell`.
+External prerequisites: firefox, geckodriver
+
+## Usage
+```
+Usage: cache-preload.py [OPTIONS] URL
+
+Options:
+  -d, --desktop / -nd, --no-desktop
+                                  enable desktop browser  [default: True]
+  -m, --mobile / -nm, --no-mobile
+                                  enable mobile browser  [default: False]
+  -gp, --geckodriver-path PATH    path to geckodriver binary  [default:
+                                  /usr/bin/geckodriver]
+  -sd, --screenshot-dir DIRECTORY
+                                  save screenshots to directory
+  -ld, --log-dir DIRECTORY        save logs to directory
+  -h, --help                      Show this message and exit.
+```
+
+This program parses the specified site maps and stores all included urls while also
+querying child site maps. Each url will then be visited by each enabled browser, including
+taking screenshots if desired. 
+
+The only argument required for operation is `URL`, which expects a site map.
+
+By default, only a desktop browser (window size 1920x1080) is used, using the `--mobile` flag
+also enables an emulated mobile browser (iPhone X user-agent, window size 375x633). To only
+use one of those browsers use the `--no-desktop` or `--no-mobile` flags respectively.
+
+Another option is the `--geckodriver-path`, which allows you to specify the location of the geckodriver binary.
+The default, however, should work on most systems.
+
+The option `--screenshot-dir` allows you to specify a directory where screenshots will be saved to. No screenshots
+will be created if omitted.
+
+Lastly, `--log-dir` specifies the directory where Firefox logs are saved to. This too is disabled if omitted.
--- cache-preload.py
+++ cache-preload.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import sys
 import time
 import xml.etree.ElementTree as ET
 from pathlib import Path
@@ -13,30 +12,41 @@ from selenium.webdriver.firefox.options import Options
 from slugify import slugify
 
 mobile_useragent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1'
+mobile_window_size = [375, 633]
+
+desktop_useragent = None
+desktop_window_size = [1920, 1080]
 
 
 def get_urls(url, max_depth=32, current_depth=1):
     urls = []
     with requests.get(url, stream=True, timeout=10, allow_redirects=True) as r:
-        # only use status 200
-        if r.status_code == 200:
-            root = ET.fromstring(r.text)
-            for item in root:
-                if item.tag.endswith('sitemap'):
-                    # sitemap
-                    if current_depth > max_depth:
-                        print('WARNING: maximum depth of {:d} reached - will not continue to search for site maps'.format(current_depth))
-                        break
+        try:
+            # only use status 200
+            if r.status_code == 200:
+                root = ET.fromstring(r.text)
+                for item in root:
+                    if item.tag.endswith('sitemap'):
+                        # sitemap
+                        if current_depth > max_depth:
+                            print('==> warning: maximum depth of {:d} reached - will not continue to search for site maps'.format(current_depth))
+                            break
 
-                    for prop in item:
-                        if prop.tag.endswith('loc'):
-                            urls += get_urls(prop.text, max_depth, current_depth + 1)
-                elif item.tag.endswith('url'):
-                    # url list
-                    for prop in item:
-                        if prop.tag.endswith('loc'):
-                            urls.append(prop.text)
-    return sorted(set(urls))
+                        for prop in item:
+                            if prop.tag.endswith('loc'):
+                                urls += get_urls(prop.text, max_depth, current_depth + 1)
+
+                    elif item.tag.endswith('url'):
+                        # url list
+                        for prop in item:
+                            if prop.tag.endswith('loc'):
+                                urls.append(prop.text)
+            else:
+                print('==> "{:s}" returned status {:d}, skipping'.format(url, r.status_code))
+
+            return sorted(set(urls))
+        except (requests.RequestException, requests.ConnectionError, requests.Timeout) as e:
+            print('==> "{:s}" failed with error "{:s}", skipping'.format(url, str(e)))
 
 
 def do_test(browser, name, url, screenshot_dir):
@@ -66,7 +76,7 @@ def get_browser(user_agent, window_size, geckodriver_path, log_path):
     if user_agent:
         browser_options.set_preference('general.useragent.override', user_agent)
 
-    browser = webdriver.Firefox(log_path=str(log_path), firefox_options=browser_options, executable_path=str(geckodriver_path))
+    browser = webdriver.Firefox(log_path=str(log_path) if log_path else None, firefox_options=browser_options, executable_path=str(geckodriver_path))
 
     browser.set_window_size(window_size[0], window_size[1])
     return browser
@@ -74,74 +84,79 @@ def get_browser(user_agent, window_size, geckodriver_path, log_path):
 
 @click.command(context_settings={'help_option_names': ['--help', '-h'], 'max_content_width': 128})
 @click.argument('url', type=click.STRING)
-@click.option('--desktop/--no-desktop', '-d/-nd', is_flag=True, default=True, show_default=True, help='use dekstop browser')
-@click.option('--mobile/--no-mobile', '-m/-nm', is_flag=True, default=False, show_default=True, help='use mobile browser')
-@click.option('--screenshots', '-s', is_flag=True, default=False, show_default=True, help='make screenshots')
+@click.option('--desktop/--no-desktop', '-d/-nd', is_flag=True, default=True, show_default=True, help='enable desktop browser')
+@click.option('--mobile/--no-mobile', '-m/-nm', is_flag=True, default=False, show_default=True, help='enable mobile browser')
 @click.option(
     '--geckodriver-path',
     '-gp',
-    type=click.Path(exists=True, file_okay=True, dir_okay=False),
+    type=click.Path(file_okay=True, dir_okay=False),
     default='/usr/bin/geckodriver',
     show_default=True,
-    help='path to geckodriver'
+    help='path to geckodriver binary'
 )
-@click.option(
-    '--screenshot-dir',
-    '-sd',
-    type=click.Path(exists=True, file_okay=False, dir_okay=True),
-    default='./screenshots',
-    show_default=True,
-    help='where to save screenshots'
-)
-@click.option(
-    '--log-dir', '-ld', type=click.Path(exists=True, file_okay=False, dir_okay=True), default='./logs', show_default=True, help='where to save logs'
-)
-def main(mobile, desktop, url, screenshots, geckodriver_path, screenshot_dir, log_dir):
+@click.option('--screenshot-dir', '-sd', type=click.Path(exists=True, file_okay=False, dir_okay=True), help='save screenshots to directory')
+@click.option('--log-dir', '-ld', type=click.Path(exists=True, file_okay=False, dir_okay=True), help='save logs to directory')
+def main(mobile, desktop, url, geckodriver_path, screenshot_dir, log_dir):
     browsers = []
 
     if desktop:
-        browsers.append({'name': 'desktop', 'window_size': [1920, 1080], 'user_agent': None})
+        browsers.append({'name': 'desktop', 'window_size': desktop_window_size, 'user_agent': desktop_useragent})
     if mobile:
-        browsers.append({'name': 'mobile', 'window_size': [375, 633], 'user_agent': mobile_useragent})
+        browsers.append({'name': 'mobile', 'window_size': mobile_window_size, 'user_agent': mobile_useragent})
 
     if len(browsers) <= 0:
-        print('no browsers enabled')
+        print('=> error: no browsers enabled')
         exit(1)
 
-    print('=> active browsers: {:s}'.format(', '.join(list(map(lambda x: x['name'], browsers)))))
+    print('=> active browsers: "{:s}"'.format(', '.join(list(map(lambda x: x['name'], browsers)))))
 
     geckodriver_path = Path(geckodriver_path).resolve()
 
-    log_dir = Path(log_dir).resolve()
-    log_dir.mkdir(parents=True, exist_ok=True)
+    if log_dir:
+        log_dir = Path(log_dir).resolve()
+        log_dir.mkdir(parents=True, exist_ok=True)
+        print('=> will save logs to "{}"'.format(str(log_dir)))
+    else:
+        log_dir = None
 
-    if screenshots:
+    if screenshot_dir:
         screenshot_dir = Path(screenshot_dir).resolve().joinpath(str(int(time.time())))
         screenshot_dir.mkdir(parents=True, exist_ok=True)
+        print('=> will save screenshots to "{}"'.format(str(screenshot_dir)))
     else:
         screenshot_dir = None
 
-    print('=> fetching all urls from sitemap on {} and its children...'.format(url), end='', flush=True)
+    print('=> fetching all urls from sitemap on "{:s}" and its children...'.format(url))
     urls = get_urls(url)
-    print(' found {:d} urls'.format(len(urls)))
+
+    if len(urls) <= 0:
+        print('\n=> error: No urls found, exiting')
+        exit(1)
+
+    print('==> found {:d} urls'.format(len(urls)))
 
     for b in browsers:
         print('=> initializing firefox...')
-        browser = get_browser(b['user_agent'], b['window_size'], geckodriver_path, log_dir.joinpath(b['name']))
+        log_path = None
+
+        if log_dir:
+            log_path = log_dir.joinpath('{:s}.log'.format(b['name']))
+
+        browser = get_browser(b['user_agent'], b['window_size'], geckodriver_path, log_path)
 
         for url in urls:
             try:
-                print('=> loading {:s} with {:s} browser...'.format(url, b['name']), end='', flush=True)
+                print('=> visiting "{:s}" with browser "{:s}" ...'.format(url, b['name']), end='', flush=True)
 
                 do_test(browser, b['name'], url, screenshot_dir)
 
                 print(' done')
             except Exception as e:
-                print('ERROR: {:s}', str(e))
+                print('\n==> Error: "{:s}"\n==> skipping "{:s}"'.format(str(e), url))
 
 
 if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt as e:
-        sys.exit(1)
+        exit(1)
